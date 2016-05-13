1a21c40bf61ddb9324239c446087e49c105186d3
Eric Amundson <eaamund@gmail.com>
2016-05-13 10:19:57 -0500

    Reporting updates

--- com.pacgenesis.ucd.util/src/com/pacgenesis/ucd/util/processrequest/ProcessRequestReport.groovy
+++ com.pacgenesis.ucd.util/src/com/pacgenesis/ucd/util/processrequest/ProcessRequestReport.groovy
@@ -3,6 +3,8 @@ package com.pacgenesis.ucd.util.processrequest
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import groovy.json.*;
+import java.nio.file.Paths
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -16,7 +18,8 @@ class ProcessRequestReport {
 		ops.addOption(Option.builder("weburi").hasArg().argName("uri").required().desc("UCD URI").build());
 		ops.addOption(Option.builder("userid").hasArg().argName("userid").required().desc("user logon id").build());
 		ops.addOption(Option.builder("password").hasArg().argName("Password").required().desc("UCD user password").build());
-		ops.addOption(Option.builder("requestId").hasArg().argName("requestId").required().desc("Process RequestId").build());
+		ops.addOption(Option.builder("requestId").hasArg().argName("requestId").desc("Process RequestId").build());
+		ops.addOption(Option.builder("fileName").hasArg().argName("fileName").desc("JSON File Name").build());
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +30,7 @@ class ProcessRequestReport {
 		String userid = null;
 		String password = null;
 		String requestId = null;
+		String fileName = null;
 		CommandLineParser parser = new DefaultParser();
 		try {
 			// parse the command line arguments
@@ -43,6 +47,9 @@ class ProcessRequestReport {
 			if (line.hasOption("requestId")) {
 				requestId = line.getOptionValue("requestId");
 			}
+			if (line.hasOption("fileName")) {
+				fileName = line.getOptionValue("fileName");
+			}
 		}
 		catch (org.apache.commons.cli.ParseException e) {
 			// TODO Auto-generated catch block
@@ -55,11 +62,23 @@ class ProcessRequestReport {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		def client = new ProcessRequestClient(uri,userid,password);
-		def request = client.getProcessRequest(requestId);
-		request.children.each { child ->
-			println "${child.displayName}";
+		def request = null;
+		if (requestId != null) {
+			def client = new ProcessRequestClient(uri,userid,password);
+			request = client.getProcessRequest(requestId);
+		} 
+		if (fileName != null) {
+			JsonSlurper slurper = new JsonSlurper();
+			String json = new File(fileName).text;
+			request = slurper.parseText(json);
 		}
+		def sList = request.children.sort { it.graphPosition };
+		sList.each { child ->
+			if (!child.displayName.equals("Join"))
+				println "${child.displayName}";
+		}
+		
+		
 	}
 
 }
